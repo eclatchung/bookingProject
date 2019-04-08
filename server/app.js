4a7da3abdc0a6cca072c10a63d1a2a01a50e823e
eclatchung <enrichir97@gmail.com>
2019-04-08 20:51:48 +0900

    nodejs mariadb connect

---
+++ server/app.js
@@ -0,0 +1,49 @@
+
+
+/*************************** 
+           EXPRESS
+**************************/
+var express = require('express');
+var app = express();
+app.use(express.json()); //instead of body-parser
+var mysql = require('mysql');
+
+/*************************
+        SOCKET.IO
+**************************/
+var http = require('http').Server(app);
+var io =require('socket.io')(http);
+
+
+//test
+app.get('/',(res)=> {
+    res.send('HI')
+    console.log('in main page')
+});
+
+//variables
+const port = process.env.PORT || 3000;
+const host = 'localhost';
+//config file database
+//var config = require('./config/config');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'user_booking',
+    password : 'booking01',
+    database : 'bookingproject'
+});
+
+//into pages
+//app.use('/api',require('./routes/api'))
+
+//socket
+io.on('connection', function(socket){
+    console.log('a user conneted');
+});
+//database
+connection.connect((err)=>{
+    if(err) console.log(err);
+    else console.log("DB connected");
+});
+//express.js
+http.listen(port,(res,req)=>console.log('node js on port 3000!'))
---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+module.exports=(()=>{
+    return {
+        connectionLimit :5,
+        host : 'localhost',
+        user : 'user_booking',
+        password:'bookingdb01',
+        database: 'bookingproject',
+        port : 3307
+    }
+})
